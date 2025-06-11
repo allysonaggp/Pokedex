c7b662e784915ab7717e400c0f5b9d525d57942c
allysonaggp <allysonaggp@gmail.com>
2025-06-11 18:44:55 -0300

    adicionado banner e mudança no layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 const pokemonList = document.getElementById('PokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5
+const maxRecord = 151
+const limit = 16
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
             <li class="pokemon ${pokemon.type}" >
-                <span class="number">${pokemon.number}</span>
+                <span class="number">#00${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 
                 <div class="detail">
@@ -30,5 +31,16 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+    const qtdRecordNextPage = offset + limit
+    
+    if (qtdRecordNextPage >= maxRecord) {
+        const newLimit = maxRecord - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+
+    
 })
