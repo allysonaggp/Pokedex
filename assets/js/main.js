72f6fb40fea9d7687c6ac1173ac1df2016bee6eb
allysonaggp <allysonaggp@gmail.com>
2025-06-11 15:31:44 -0300

    criando botao de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,34 @@
+const pokemonList = document.getElementById('PokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0;
 
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}" >
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                    ${pokemon.types.map((type) => `<li class ="type ${type}">${type}</li>`).join('')}       
+                    </ol>
 
-function convertPokemonToList(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}" >
-        <span class="number">${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        
-        <div class="detail">
-            <ol class="types">
-            ${pokemon.types.map((type) => `<li class ="type ${type}">${type}</li>`).join('')}       
-            </ol>
+                    <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
 
-            <img src="${pokemon.photo}" 
-            alt="${pokemon.name}">
-
-        </div>                
-    </li>
-    `
+                </div>                
+            </li>
+            `
+        ).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('PokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-
-    const newHtml = pokemons.map(convertPokemonToList).join('')
-    pokemonList.innerHTML = newHtml
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
-
-
