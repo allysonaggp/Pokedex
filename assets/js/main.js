6aba4c9a4b1072bbbe9426326271fd46034121bc
allysonaggp <allysonaggp@gmail.com>
2025-06-11 13:42:58 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
-function convertPokemonTypesToList(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+
 
 function convertPokemonToList(pokemon) {
     return `
-    <li class="pokemon">
-        <span class="number">${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}" >
+        <span class="number">${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         
         <div class="detail">
             <ol class="types">
-            ${convertPokemonTypesToList(pokemon.types).join('')}       
+            ${pokemon.types.map((type) => `<li class ="type ${type}">${type}</li>`).join('')}       
             </ol>
 
-            <img src="${pokemon.sprites.other.dream_world.front_default}" 
+            <img src="${pokemon.photo}" 
             alt="${pokemon.name}">
 
         </div>                
